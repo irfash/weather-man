bed482509a3e5a20e920795edf64ce195d1ad119
irfash <irfashfarook@gmail.com>
2022-09-10 12:10:40 +0530

    erdited gitignore

--- src/constant.js
+++ src/constant.js
@@ -1,10 +1,10 @@
+import { KEY } from "./config";
 
-export const URL ={
-    
-    BASE :"https://api.openweathermap.org/data/2.5",
-    KEY:"51d1c9a4ceff34d876a0f4a1eef28ad2",
-    EVENTS:{
-        API_ID:"&appid=",
-        QUERI:"weather?q="
-    }    
-}
+export const URL = {
+  BASE: "https://api.openweathermap.org/data/2.5",
+  KEY: { KEY },
+  EVENTS: {
+    API_ID: "&appid=",
+    QUERI: "weather?q=",
+  },
+};
